c4b94f8aff9872b42454ea214315dea607cd23b8
MarcoIeni <marcoieni94@gmail.com>
2019-08-21 16:48:59 +0200

    JsonSettingsRepositoryTest: added positive test for isCacheValid function

--- src/test/kotlin/data/persistence/settings/JsonSettingsRepositoryTest.kt
+++ src/test/kotlin/data/persistence/settings/JsonSettingsRepositoryTest.kt
@@ -4,12 +4,53 @@ import data.persistence.PersistenceFilePaths
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.TestTemplate
 import repositories.settings.ProjectProperties
 import repositories.settings.Settings
+import repositories.settings.SettingsRepository
 import repositories.settings.UserProperties
+import java.io.File
+import java.time.LocalDateTime
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class JsonSettingsRepositoryTest {
+
+    @Test
+    fun checkRecentlyCreatedCache_isValid() {
+        val recentCacheFilepath = "src/test/resources/cache/recentCache.json"
+        val now = LocalDateTime.now()
+        val recentCacheFileContent = """
+            {
+              "timestamp": {
+                "date": {
+                  "year": ${now.year},
+                  "month": ${now.monthValue},
+                  "day": ${now.dayOfMonth}
+                },
+                "time": {
+                  "hour": ${now.hour},
+                  "minute": ${now.minute},
+                  "second": ${now.second},
+                  "nano": ${now.nano}
+                }
+              },
+              "stats": []
+            }
+        """.trimIndent()
+        val recentCacheFile = File(recentCacheFilepath)
+        recentCacheFile.writeText(recentCacheFileContent)
+        val settingsRepository = JsonSettingsRepository(PersistenceFilePaths(
+            cache = recentCacheFilepath,
+            settings = "src/test/resources/settings/cache_exp_time_10.json"
+        ))
+        val fiveSecondsFromNow = now.plusSeconds(5)
+        val isCacheValid = settingsRepository.isCacheValid(fiveSecondsFromNow)
+        // cache expiry time is 10 seconds and only 5 seconds have passed
+        // since cache was saved, so the cache should be still valid
+        assert(isCacheValid)
+
+        recentCacheFile.delete()
+    }
 
     @Test
     fun readSettingsFile_ConstructProperObject() {
